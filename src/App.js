1716ed9954833db38d4f7cf6922727ab97350464
James Pesenti <74566532+JamesPesenti@users.noreply.github.com>
2022-09-03 15:25:39 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,37 +26,6 @@ function App() {
           <p><a href="" target="_parent">&lt; Back To Top</a></p>
         </div>
       </div>
-
-
-
-
-
-{/* <section class="parallax-group the-office">
-  <div class="parallax-layer background">
-    <div class="background-container">
-      <div class="background-artwork"></div>
-    </div>
-    <div class="content">
-      <h3>Watch all 9 seasons of <em>The Office</em> four times through.</h3>
-    </div>
-  </div>
-</section>
-<section class="parallax-group pieces the-office-pieces">
-  <div class="parallax-layer foreground layer-1">
-    <img src="/images/the-office-jello.svg" width="237" height="148" alt="phone1" class="jello" />
-  </div>
-  <div class="parallax-layer foreground layer-2">
-    <img src="/images/paper-airplane.svg" width="173" height="215" alt="phone2" class="airplane" />
-  </div>
-  <div class="parallax-layer foreground layer-3">
-    <img src="/images/the-office-mug.svg" width="160" height="145" alt="phone3" class="mug" />
-  </div>
-  <div class="parallax-layer foreground layer-2">
-    <img src="/images/the-office-beet.svg" width="200" height="274" alt="phone4" class="beet" />
-  </div>
-</section>
-<section class="parallax-group space-mountain">
-</section> */}
     </>
     );
 }
