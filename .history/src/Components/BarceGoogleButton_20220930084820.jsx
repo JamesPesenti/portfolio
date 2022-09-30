8e36d144f6bc1bb758580a30121d8dc967a785e6
James Pesenti <you@example.com>
2022-09-30 09:00:06 -0400

    barce button

--- .history/src/Components/BarceGoogleButton_20220930084820.jsx
+++ .history/src/Components/BarceGoogleButton_20220930084820.jsx
@@ -6,7 +6,7 @@ const BarceGoogleButton = () => {
     <>
     <button type="button" style={{marginLeft: 30, borderWidth: .1, borderColor: '#efefef', borderRadius: 5}}>
         <a href="https://play.google.com/store/apps/details?id=com.jamespesenti91.VisitBarcelona">
-            <img style={{width: 100, height: 50, borderRadius: 0}} src="https://logos-world.net/wp-content/uploads/2020/12/Google-Play-Logo-500x281.png" alt="barceButton"/>
+            <img style={{width: 100, height: 50, borderRadius: 0}} src="https://logos-world.net/wp-content/uploads/2020/12/Google-Play-Logo-500x281.png"/>
         </a>
     </button>
     </>
